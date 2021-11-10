8ae91efc4cd6a31766250fd5258373c54ba4e3f1
UwUBeebi <chris.erman@khk.ee>
2021-11-10 09:59:03 +0200

    Fixed another styling mistake

--- classes/UI.js
+++ classes/UI.js
@@ -4,13 +4,12 @@ class UI {
 		this.temp = document.querySelector('#temp')
 		this.city = document.querySelector('#location')
 	}
-
-	drawWeather( data ) {
+	drawWeather(data) {
   		let celcius = Math.round(parseFloat(data.main.temp)-273.15); 
   		let description = data.weather[0].description;
   
   		this.description.innerHTML = description;
-  		this.temp.innerHTML = celcius + '&deg;';
+  		this.temp.innerHTML = celcius + '&deg';
   		this.city.innerHTML = data.name;
 	}
 }
